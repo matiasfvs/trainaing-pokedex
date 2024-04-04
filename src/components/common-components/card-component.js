4c693c5364e9c49b08e98c4cb2c69c1563a51c23
matiasfvs <valenzuela.solorza.m@gmail.com>
2024-04-04 18:48:41 -0300

    Update card-component.js

--- src/components/common-components/card-component.js
+++ src/components/common-components/card-component.js
@@ -6,40 +6,18 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-cardComponentViewer = () => {
-    const [dataUrl, setDataUrl] = useState()
-
-    const fetchPokemon = () => {
-        setLoading(true);
-        axios.get('https://pokeapi.co/api/v2/pokemon?limit=100')
-          .then(response => {
-            setDataPokemon(response.data.results);
-            setLoading(false);
-          })
-          .catch(error => {
-            console.error('Error fetching Pokemon list:', error);
-            setLoading(false);
-          });
-      };
-
-
-    return {
-        setDataUrl,
-    }
-
-}
 
 
 const CardPokemon = ({ pokemonData })=> {
 
-  const {setDataUrl} = cardComponentViewer()
+ // const {setDataUrl} = cardComponentViewer()
 
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           {JSON.stringify(pokemonData.name)}
-          {setDataUrl(pokemonData.url)}
+          {/* {setDataUrl(pokemonData.url)} */}
         </Typography>
       </CardContent>
       <CardActions>
