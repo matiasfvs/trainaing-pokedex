192340cc5210a27a4053f3796eaf9ac60978aeab
matiasfvs <valenzuela.solorza.m@gmail.com>
2024-04-04 17:27:28 -0300

    Api Pokemon
    - add pokeApi

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,13 +1,43 @@
-import React from 'react'
+
+import React,{useEffect, useState} from 'react'
 import {Container} from '@mui/material'
+import axios from 'axios'
 
+const homeViewer = () => {
+  const [dataPokemon, setDataPokemon] = useState();
+  const [loading, setLoading] = useState(false);
 
+  const fetchPokemonList = () => {
+    setLoading(true);
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=100')
+      .then(response => {
+        setDataPokemon(response.data.results);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching Pokemon list:', error);
+        setLoading(false);
+      });
+  };
+
+  // Retornar un objeto con las variables y funciones
+  return {
+    dataPokemon,
+    loading,
+    fetchPokemonList
+  };
+};
 
 const Home = () => {
+ const {dataPokemon,fetchPokemonList} = homeViewer()
+
+ useEffect(() => {
+  fetchPokemonList()
+ },[])
 
   return(
   <Container>
-
+    {JSON.stringify(dataPokemon)}
   </Container>
   )
 
