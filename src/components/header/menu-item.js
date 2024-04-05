2c82ee0bf5c2c7210cfbf26d3ed7c61f28fd85a7
HowlDevs <157731912+HowlDevs@users.noreply.github.com>
2024-04-04 21:22:41 -0300

    commit xxxxx
    xxxxxxxdx

--- src/components/header/menu-item.js
+++ src/components/header/menu-item.js
@@ -29,7 +29,7 @@ const MenuPag = () => {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              POKEDEX
+              POKEDEXxxxxxxxxxxxxxxxx
             </Typography>
             <Button color="inherit">{t('menuItemFeatures.Login')}</Button>
           </Toolbar>
