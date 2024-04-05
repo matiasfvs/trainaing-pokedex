e91ac46a87b4dffc6b8ab5a83b4cddd64fbb08e3
HowlDevs <157731912+HowlDevs@users.noreply.github.com>
2024-04-04 23:06:17 -0300

    commit SearchBar 3.0
    SearchBar actualizado con controlador ENTER y con controlador de actualizar al borrar los datos.

--- src/components/common-components/search-component.js
+++ src/components/common-components/search-component.js
@@ -2,15 +2,22 @@ import React, { useState } from 'react';
 import { TextField, Button } from '@mui/material';
 
 const SearchBar = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState(''); // searchTerm definido como variable de estado y setSearchTerm para actualizar el estado del searchTerm (al momento de buscar pokémon)
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSearch = () => { // La variable handleSearch maneja la búsqueda y filtra los datos y actualiza el estado de la constante filteredData especificada en el Home
+  const handleSearch = () => {
     onSearch(searchTerm);
   };
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      handleSearch(); // Llama a handleSearch cuando se presiona la tecla Enter
+      handleSearch();
+    }
+  };
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+    if (e.target.value === '') { // Si el campo de búsqueda está vacío llama a onSearch con una cadena vacía para mostrar todos los Pokémon
+      onSearch('');
     }
   };
 
@@ -19,8 +26,8 @@ const SearchBar = ({ onSearch }) => {
       <TextField 
         label="Buscar Pokémon"
         value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        onKeyDown={handleKeyPress} // Evento del onKeyDown para apretar enter y que busque
+        onChange={handleChange} // Agrega el controlador de eventos para el evento onChange
+        onKeyDown={handleKeyPress}
       />
       <Button variant="contained" onClick={handleSearch}>Buscar</Button>
     </div>
@@ -28,4 +35,3 @@ const SearchBar = ({ onSearch }) => {
 };
 
 export default SearchBar;
-
